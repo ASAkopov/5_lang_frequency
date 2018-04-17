0a332ef73376dba74f9ccc0211509cfbca04b56a
ASAkopov <asakopov@gmail.com>
2018-04-17 12:44:10 +0300

    Adds variable words_num specificating number of output words.

--- lang_frequency.py
+++ lang_frequency.py
@@ -7,7 +7,7 @@ def load_data(filepath):
         return file.read()
 
 
-def get_most_frequent_words(text_string):
+def get_most_frequent_words(text_string, words_num):
     text_string.lower()
     words = re.findall(r"[a-zа-яё]+", text_string)
 
@@ -15,10 +15,11 @@ def get_most_frequent_words(text_string):
     for word in words:
         words_counter[word] += 1
 
-    return list(map(lambda a: a[0], words_counter.most_common(10)))
+    return list(map(lambda a: a[0], words_counter.most_common(words_num)))
 
 
 if __name__ == '__main__':
+    words_num = 10
     text_file = input("Enter textfile path: ")
     text = load_data(text_file)
     print(get_most_frequent_words(text))
