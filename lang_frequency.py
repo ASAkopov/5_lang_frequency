43b109b23c55a5376ceed16811b0ca651a7b5cd1
ASAkopov <asakopov@gmail.com>
2018-04-21 00:24:02 +0300

    fix with recoms

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,25 +1,28 @@
 import re
 from collections import Counter
+from sys import argv, exit
 
 
 def load_data(filepath):
-    with open(filepath, "r", encoding = "cp1251") as file:
+    with open(filepath, 'r') as file:
         return file.read()
 
 
 def get_most_frequent_words(text_string, words_num):
     text_string.lower()
-    words = re.findall(r"[a-zа-яё]+", text_string)
+    words = re.findall(r'[a-zа-яё]+', text_string)
 
-    words_counter = Counter()
-    for word in words:
-        words_counter[word] += 1
-
+    words_counter = Counter(words)
     return list(map(lambda a: a[0], words_counter.most_common(words_num)))
 
 
 if __name__ == '__main__':
-    words_num = 10
-    text_file = input("Enter textfile path: ")
-    text = load_data(text_file)
-    print(get_most_frequent_words(text, words_num))
+    if len(argv) == 1:
+       exit('Filepath was not specified.')
+    file_path = argv[1]
+    try:
+        text = load_data(file_path)
+    except IOError:
+        print('Can not open file.')
+    else:
+        get_most_frequent_words(text)
