1d4f8fd9dd886c15031456f4e78db604cb7a970c
ASAkopov <asakopov@gmail.com>
2018-04-22 23:29:00 +0300

    22.04.2018

--- lang_frequency.py
+++ lang_frequency.py
@@ -19,10 +19,15 @@ def get_most_frequent_words(text_string, words_num):
 if __name__ == '__main__':
     if len(argv) == 1:
        exit('Filepath was not specified.')
+
     file_path = argv[1]
+    if len(argv) < 3:
+        words_num = 10
+    else:
+        words_num = argv[2]
     try:
         text = load_data(file_path)
     except IOError:
         print('Can not open file.')
     else:
-        get_most_frequent_words(text)
+        get_most_frequent_words(text, words_num)
