75b64bb857dc30ebf0870e1d8a6ad238cabd1716
ASAkopov <asakopov@gmail.com>
2018-04-25 23:25:17 +0300

    25.04.2018 simplify code

--- lang_frequency.py
+++ lang_frequency.py
@@ -16,21 +16,20 @@ def get_most_frequent_words(text_string, words_num):
     words = re.findall(r'[a-zа-яё]+', text_string)
 
     words_counter = Counter(words)
-    return list(map(lambda a: a[0], words_counter.most_common(words_num)))
+    return words_counter.most_common(words_num)
 
 
 if __name__ == '__main__':
     if len(argv) == 1:
        exit('Filepath was not specified.')
-
     file_path = argv[1]
     if len(argv) < 3:
         words_num = 10
     else:
-        words_num = argv[2]
+        words_num = int(argv[2])
     try:
         text = load_data(file_path)
     except IOError:
         print('Can not open file.')
     else:
-        get_most_frequent_words(text, words_num)
+        print(get_most_frequent_words(text, words_num))
